102625e1dc2824fce46c8870ee5683630b28fda8
AlimBodzhgua <bodzhgua2015@mail.ru>
2026-02-20 21:52:25 +0300

    added antfu eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,32 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import antfu from '@antfu/eslint-config';
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
+export default antfu({
+	nextjs: true,
+	typescript: true,
+	jsonc: false,
+	yaml: false,
 
-export default eslintConfig;
+	rules: {
+		'antfu/top-level-function': 'off',
+		'ts/consistent-type-definitions': 'off',
+		'node/prefer-global/process': 'off',
+
+		'style/no-tabs': 'off',
+		'style/arrow-parens': ['error', 'always'],
+		'style/jsx-quotes': ['error', 'prefer-single'],
+		'style/jsx-indent-props': ['error', 'tab'],
+		'style/indent': ['off', 'tab'],
+		'style/indent-binary-ops': ['off', 'tab'],
+		'style/brace-style': ['error', '1tbs', { allowSingleLine: true }],
+
+		'perfectionist/sort-named-imports': ['warn', {
+			type: 'alphabetical',
+		}],
+		'perfectionist/sort-named-exports': ['warn', {
+			type: 'alphabetical',
+		}],
+	},
+	stylistic: {
+		semi: true,
+	},
+});
