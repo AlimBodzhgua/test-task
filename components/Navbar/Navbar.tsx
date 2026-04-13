0e8c4a07dbb27d1c50aed6529cf3958bd184233e
AlimBodzhgua <bodzhgua2015@mail.ru>
2026-04-13 14:05:13 +0300

    added useMediaQuery hook, added conditional rendering on mobile navbar items

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -4,13 +4,15 @@ import type { SectionType } from './types';
 
 import { useEffect, useState } from 'react';
 import { cn } from '@/snared/lib/utils';
+import { useMediaQuery } from '@/snared/hooks/useMediaQuery';
 import { navbarList, sections } from './constants';
-import { NavbarItem } from './NavbarItem';
 import { MobileNavbarItem } from './MobileNavbarItem';
+import { NavbarItem } from './NavbarItem';
 import { BlurFade } from '../ui';
 
 export function Navbar() {
 	const [activeSection, setActiveSection] = useState<SectionType>();
+	const isMobile = useMediaQuery('(max-width: 640px)');
 
 	useEffect(() => {
 		const observer = new IntersectionObserver(
@@ -32,40 +34,56 @@ export function Navbar() {
 	}, []);
 
 	return (
-		<BlurFade duration={0.7} direction='down' className='fixed bottom-0 sm:top-0 right-0 left-0 z-50'>
+		<BlurFade
+			duration={0.7}
+			direction={isMobile ? 'up' : 'down'}
+			className='fixed bottom-0 sm:top-0 right-0 left-0 z-50'
+		>
 			<nav
-				className={cn(`
-					row mx-auto mt-6 mb-6 flex max-w-90 sm:max-w-105 md:max-w-155 items-center justify-center md:justify-between rounded-full bg-gray-950 px-4 py-3 sm:px-7 sm:py-4 transition-all`,
-					activeSection !== 'home' && `backdrop-blur-md border-2 border-gray-500 bg-gradient-to-br from-white/20 to-white/5 shadow-lg`)}
+				className={cn(
+					` row mx-auto mt-6 mb-6 flex max-w-90 sm:max-w-105 md:max-w-155
+					items-center justify-center md:justify-between rounded-full bg-gray-950
+					px-4 py-3 sm:px-7 sm:py-4 transition-all`,
+					activeSection !== 'home' &&
+						`backdrop-blur-md border-2 border-gray-500 bg-gradient-to-br
+						from-white/20 to-white/5 shadow-lg`,
+				)}
 			>
 				<div className='hidden md:block'>
-					<h1 className={cn('text-xl font-bold text-white transition-colors', activeSection !== 'home' && 'text-slate-400')}>
+					<h1
+						className={cn(
+							'text-xl font-bold text-white transition-colors',
+							activeSection !== 'home' && 'text-slate-400',
+						)}
+					>
 						AB
 						<span className='text-blue-500 font-extrabold ml-1'>.</span>
 					</h1>
 				</div>
-				<div className='hidden sm:flex items-center justify-between gap-3'>
-					{navbarList.map((item) => (
-						<NavbarItem
-							key={item.section}
-							Icon={item.Icon}
-							section={item.section}
-							text={item.text}
-							activeSection={activeSection}
-						/>
-					))}
-				</div>
-
-				<div className='flex sm:hidden items-center justify-center gap-1.5'>
-					{navbarList.map((item) => (
-						<MobileNavbarItem
-							key={item.section}
-							Icon={item.Icon}
-							section={item.section}
-							activeSection={activeSection}
-						/>
-					))}
-				</div>
+				{isMobile ? (
+					<div className='flex items-center justify-center gap-1.5'>
+						{navbarList.map((item) => (
+							<MobileNavbarItem
+								key={item.section}
+								Icon={item.Icon}
+								section={item.section}
+								activeSection={activeSection}
+							/>
+						))}
+					</div>
+				) : (
+					<div className='flex items-center justify-between gap-3'>
+						{navbarList.map((item) => (
+							<NavbarItem
+								key={item.section}
+								Icon={item.Icon}
+								section={item.section}
+								text={item.text}
+								activeSection={activeSection}
+							/>
+						))}
+					</div>
+				)}
 			</nav>
 		</BlurFade>
 	);
